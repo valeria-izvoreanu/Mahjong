f0762d1a183c0e96119b121d49f9768d559a30ba
valeria izvoreanu <valeria.izvoreanu@gmail.com>
2021-12-23 19:42:30 +0200

    add some constants

---
+++ mahjong/constants.py
@@ -0,0 +1,18 @@
+import pygame.font
+
+width = 1070
+height = 720
+
+white = (255, 255, 255)
+color_light = (170, 170, 170)
+color_dark = (100, 100, 100)
+slate_grey = (112, 128, 144)
+light_grey = (165, 175, 185)
+blackish = (10, 10, 10)
+black = (0, 0, 0)
+green = (102, 205, 0)
+pink = (205, 91, 69)
+blue = (0, 205, 205)
+light_pink = (255, 127, 80)
+
+font_type = 'Corbel'
